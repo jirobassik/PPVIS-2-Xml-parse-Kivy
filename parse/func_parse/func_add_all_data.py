9c2d85ae0080456e807140a6650658b5dcf09c8b
jirobassik <72316169+jirobassik@users.noreply.github.com>
2022-03-28 10:50:21 +0300

    Add num data

--- parse/func_parse/func_add_all_data.py
+++ parse/func_parse/func_add_all_data.py
@@ -17,7 +17,7 @@ def add_all_data():
         a = tabulate(data, headers=head, tablefmt="grid", stralign='center')
         get_mas_all().clear()
     except xml.sax._exceptions.SAXParseException:
-        a = "В xml файле есть синтаксическая ошибка"
+        a = "В xml файле есть грубая синтаксическая ошибка"
     return a
 
 def update_data():
@@ -36,8 +36,21 @@ def update_data():
         b = tabulate(data, headers=head, tablefmt="grid", stralign='center')
         get_mas_all().clear()
     except xml.sax._exceptions.SAXParseException:
-        b = "В xml файле есть синтаксическая ошибка"
+        b = "В xml файле есть грубая синтаксическая ошибка"
     return b
+
+def get_num_data():
+    parser = xml.sax.make_parser()
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+    handler = Add_All_Data()
+    parser.setContentHandler(handler)
+    try:
+        parser.parse("D:/Programs/PyCharm Community Edition 2021.2.3/Project/PPVIS2/Data.xml")
+        stroka = f"В базе данных {len(get_mas_all())} элемент"
+        get_mas_all().clear()
+    except xml.sax._exceptions.SAXParseException:
+        stroka = "В xml файле есть грубая синтаксическая ошибка"
+    return stroka
 
 def print_all_id():
     v = ""
